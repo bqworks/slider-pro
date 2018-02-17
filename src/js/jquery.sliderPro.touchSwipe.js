6da8050b2e409de9f134acee925b00a309f91a43
David Ghiurau <david.ghiurau@gmail.com>
2018-02-17 16:45:38 +0200

    improve click/tap event handling when touch-swipe is enabled

--- src/js/jquery.sliderPro.touchSwipe.js
+++ src/js/jquery.sliderPro.touchSwipe.js
@@ -50,13 +50,20 @@
 				event.preventDefault();
 			});
 
+			// Prevent 'click' events unless there is intention for a 'click'
+			this.$slidesMask.find( 'a' ).on( 'click.' + NS, function( event ) {
+				if ( typeof event.originalEvent.touches === 'undefined' && that.$slider.hasClass( 'sp-swiping' ) ) {
+					event.preventDefault();
+				}
+			});
+
 			// Add the grabbing icon
 			this.$slidesMask.addClass( 'sp-grab' );
 		},
 
 		// Called when the slides starts being dragged
 		_onTouchStart: function( event ) {
-		
+
 			// Disable dragging if the element is set to allow selections
 			if ( $( event.target ).closest( '.sp-selectable' ).length >= 1 ) {
 				return;
@@ -64,16 +71,6 @@
 
 			var that = this,
 				eventObject = typeof event.originalEvent.touches !== 'undefined' ? event.originalEvent.touches[0] : event.originalEvent;
-
-			// Prevent default behavior only for mouse events
-			if ( typeof event.originalEvent.touches === 'undefined' ) {
-				event.preventDefault();
-			}
-
-			// Disable click events on links
-			$( event.target ).parents( '.sp-slide' ).find( 'a' ).one( 'click.' + NS, function( event ) {
-				event.preventDefault();
-			});
 
 			// Get the initial position of the mouse pointer and the initial position
 			// of the slides' container
@@ -93,7 +90,7 @@
 			}
 
 			// Listen for move and end events
-			this.$slidesMask.on( this.touchSwipeEvents.moveEvent, $.proxy( this._onTouchMove, this ) );
+			$( document ).on( this.touchSwipeEvents.moveEvent, $.proxy( this._onTouchMove, this ) );
 			$( document ).on( this.touchSwipeEvents.endEvent, $.proxy( this._onTouchEnd, this ) );
 
 			// Swap grabbing icons
@@ -136,6 +133,7 @@
 				return;
 			}
 			
+			// Don't allow opposite scrolling
 			event.preventDefault();
 
 			if ( this.settings.loop === false ) {
@@ -155,25 +153,25 @@
 			var that = this,
 				touchDistance = this.settings.orientation === 'horizontal' ? this.touchDistance.x : this.touchDistance.y;
 
-			// Remove the move and end listeners
-			this.$slidesMask.off( this.touchSwipeEvents.moveEvent );
+			// Remove the 'move' and 'end' listeners
+			$( document ).off( this.touchSwipeEvents.moveEvent );
 			$( document ).off( this.touchSwipeEvents.endEvent );
+			
 			this.allowOppositeScrolling = true;
 
 			// Swap grabbing icons
 			this.$slidesMask.removeClass( 'sp-grabbing' ).addClass( 'sp-grab' );
 
-			// Check if there is intention for a tap
+			// Check if there is intention for a tap and remove
+			// the 'sp-swiping' class if that's the case
 			if ( this.isTouchMoving === false || this.isTouchMoving === true && Math.abs( this.touchDistance.x ) < 10 && Math.abs( this.touchDistance.y ) < 10 ) {
-				// Re-enable click events on links
-				$( event.target ).parents( '.sp-slide' ).find( 'a' ).off( 'click.' + NS );
 				this.$slider.removeClass( 'sp-swiping' );
 			}
 
-			// Remove the 'sp-swiping' class but with a delay
-			// because there might be other event listeners that check
-			// the existence of this class, and this class should still be 
-			// applied for those listeners, since there was a swipe event
+			// Remove the 'sp-swiping' class anyway, even if there was a swipe,
+			// but in this case remove it with a delay, because there might be 
+			// other event listeners that check the existence of this class,
+			// and this class should still be applied for those listeners
 			setTimeout(function() {
 				that.$slider.removeClass( 'sp-swiping' );
 			}, 1 );
@@ -184,10 +182,6 @@
 			}
 
 			this.isTouchMoving = false;
-
-			$( event.target ).parents( '.sp-slide' ).one( 'click', function( event ) {
-				event.preventDefault();
-			});
 
 			// Calculate the old position of the slides in order to return to it if the swipe
 			// is below the threshold
@@ -222,10 +216,13 @@
 
 		// Destroy the module
 		destroyTouchSwipe: function() {
-			this.$slidesMask.off( this.touchSwipeEvents.startEvent );
-			this.$slidesMask.off( this.touchSwipeEvents.moveEvent );
 			this.$slidesMask.off( 'dragstart.' + NS );
+			this.$slidesMask.find( 'a' ).off( 'click.' + NS );
+
+			this.$slidesMask.off( this.touchSwipeEvents.startEvent );
+			$( document ).off( this.touchSwipeEvents.moveEvent );
 			$( document ).off( this.touchSwipeEvents.endEvent );
+			
 			this.$slidesMask.removeClass( 'sp-grab' );
 		},
 
