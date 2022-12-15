15de43b4fe9371b3cd2875e745d7c0b5511c64e4
David Ghiurau <contact@bqworks.net>
2022-12-15 19:01:26 +0200

    update dist files

--- dist/js/jquery.sliderPro.js
+++ dist/js/jquery.sliderPro.js
@@ -1,8 +1,8 @@
 /*!
-*  - v1.6.0
-* Homepage: http://bqworks.net/slider-pro/
+*  - v1.6.1
+* Homepage: https://bqworks.net/slider-pro/
 * Author: bqworks
-* Author URL: http://bqworks.net/
+* Author URL: https://bqworks.net/
 */
 ;(function( window, $ ) {
 
@@ -4403,6 +4403,76 @@
 
 	$.SliderPro.addModule( 'Keyboard', Keyboard );
 	
+})( window, jQuery );
+
+;( function( window, $ ) {
+
+    "use strict";
+
+    var NS = 'MouseWheel.' + $.SliderPro.namespace;
+
+    var MouseWheel = {
+
+        mouseWheelEventType: '',
+
+        initMouseWheel: function() {
+            var that = this;
+
+            if ( this.settings.mouseWheel === false ) {
+                return;
+            }
+
+            // get the current mouse wheel event used in the browser
+            if ( 'onwheel' in document ) {
+                this.mouseWheelEventType = 'wheel';
+            } else if ( 'onmousewheel' in document ) {
+                this.mouseWheelEventType = 'mousewheel';
+            } else if ( 'onDomMouseScroll' in document ) {
+                this.mouseWheelEventType = 'DomMouseScroll';
+            } else if ( 'onMozMousePixelScroll' in document ) {
+                this.mouseWheelEventType = 'MozMousePixelScroll';
+            }
+
+            this.on( this.mouseWheelEventType + '.' + NS, function( event ) {
+                event.preventDefault();
+
+                var eventObject = event.originalEvent,
+                    delta;
+
+                // get the movement direction and speed indicated in the delta property
+                if ( typeof eventObject.detail !== 'undefined' ) {
+                    delta = eventObject.detail;
+                }
+
+                if ( typeof eventObject.wheelDelta !== 'undefined' ) {
+                    delta = eventObject.wheelDelta;
+                }
+
+                if ( typeof eventObject.deltaY !== 'undefined' ) {
+                    delta = eventObject.deltaY * -1;
+                }
+
+                if ( that.thumbnailsPosition + delta < 0 && that.thumbnailsPosition + delta > that.thumbnailsContainerSize - that.thumbnailsSize ) {
+                   that._moveThumbnailsTo( that.thumbnailsPosition + delta, true );
+                } else if ( that.thumbnailsPosition + delta >= 0 ) {
+                    that._moveThumbnailsTo( 0, true );
+                } else if ( that.thumbnailsPosition + delta < that.thumbnailsContainerSize - that.thumbnailsSize ) {
+                    that._moveThumbnailsTo( that.thumbnailsContainerSize - that.thumbnailsSize, true );
+                }
+            });
+        },
+
+        destroyMouseWheel: function() {
+            this.off( this.mouseWheelEventType + '.' + NS );
+        },
+
+        mouseWheelDefaults: {
+            mouseWheel: false
+        }
+    };
+
+    $.SliderPro.addModule( 'MouseWheel', MouseWheel );
+
 })( window, jQuery );
 
 // Full Screen module for Slider Pro.
