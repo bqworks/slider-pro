4ef406732b93ae0f68303897a8a63548c24efb4c
WPExplorer <wpexplorer@gmail.com>
2024-02-21 19:34:16 -0800

    Update jquery.sliderPro.js
    Fixes issue with slider width not being rounded while slider position is rounded causing potential issue where a tiny bit of the previous slide can be seen.

--- dist/js/jquery.sliderPro.js
+++ dist/js/jquery.sliderPro.js
@@ -604,7 +604,11 @@
 			
 			// Initially set the slide width to the size of the slider.
 			// Later, this will be set to less if there are multiple visible slides.
-			this.slideWidth = this.$slider.width();
+			if ( this.settings.fade === true ) {
+				this.slideWidth = this.$slider.width();
+		        } else {
+		        	this.slideWidth = Math.round( this.$slider.width() );
+		        }
 
 			// Set the height to the same size as the browser window if the slider is set to be 'fullWindow',
 			// or calculate the height based on the width and the aspect ratio.
